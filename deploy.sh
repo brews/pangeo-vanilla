2e23552944955e760ccb72a29f36f30cfa02ec69
Brewster Malevich <bmalevich@rhg.com>
2020-02-06 22:11:49 -0800

    Refactor workload between CI and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+gcloud auth activate-service-account --key-file=key.json
+
+gcloud config set project $PROJECT_NAME
+gcloud config set container/cluster $CLUSTER_NAME
+gcloud config set compute/zone $ZONE
+gcloud container clusters get-credentials $CLUSTER_NAME --zone $ZONE
+
 helm upgrade pangeohub pangeo/pangeo \
     --devel --version "19.09.27-86dd66c" \
     -f parameters.yaml \
