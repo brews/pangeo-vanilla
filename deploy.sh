2db436e7fbc980e66f9f7d8fe3428640743111c9
Brewster Malevich <bmalevich@rhg.com>
2020-02-06 20:03:42 -0800

    Fill out deploy script and parameters

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-echo "Hello, world!"
+helm upgrade pangeohub pangeo/pangeo \
+    --devel --version "19.09.27-86dd66c" \
+    -f parameters.yaml \
+    --set jupyterhub.proxy.secretToken=$JHUB_SECRET_TOKEN \
+    --set jupyterhub.auth.github.clientId=$GITHUB_CLIENTID \
+    --set jupyterhub.auth.github.clientSecret=$GITHUB_CLIENTSECRET \
+    --namespace=pangeo
